6fe0d857f6a975eef1e5336753b3498708f9a14a
Alex Ferreira <aferreira91@gmail.com>
2019-05-01 14:54:08 -0300

    adjusting configurations for deployment

--- Dockerfile
+++ Dockerfile
@@ -7,8 +7,8 @@ RUN pip3 install -r requirements.txt
 RUN wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -O cloud_sql_proxy
 RUN chmod +x cloud_sql_proxy
 
-ENV GOOGLE_APPLICATION_CREDENTIALS="./cloudsql/credentials.json"
+ENV GOOGLE_APPLICATION_CREDENTIALS="/opt/service_accout_key.json"
 
-EXPOSE 5000/tcp
+EXPOSE 8080/tcp
 
-ENTRYPOINT ./manage.py runserver --host 0.0.0.0
+ENTRYPOINT ./manage.py runserver --host 0.0.0.0 --port 8080
--- config.py
+++ config.py
@@ -5,25 +5,14 @@ class Config(object):
     DEBUG = "DEBUG" in os.environ
     HTTP_PORT = int(os.environ.get('PORT', 5000))
     SECRET_KEY = os.environ.get("SECRET_KEY", "unsafeKey")
-    PROJECT_ID = 'cloudufc'
-    DATABASE_USER = 'root'
-    DATABASE_PASSWORD = 'photoshare'
-    DATABASE_NAME = 'photoshare'
-    DATABASE_CONNECTION_NAME = 'cloudufc:us-central1:photoshare'
-    LOCAL_SQLALCHEMY_DATABASE_URI = (
-        'mysql+pymysql://{user}:{password}@35.232.125.214:3306/{database}').format(
-            user=DATABASE_USER, password=DATABASE_PASSWORD,
-            database=DATABASE_NAME)
-    # When running on App Engine a unix socket is used to connect to the cloudsql
-    # instance.
-    LIVE_SQLALCHEMY_DATABASE_URI = (
-        'mysql+pymysql://{user}:{password}@localhost/{database}'
-        '?unix_socket=/cloudsql/{connection_name}').format(
-            user=DATABASE_USER, password=DATABASE_PASSWORD,
-            database=DATABASE_NAME, connection_name=DATABASE_CONNECTION_NAME)
-    if os.environ.get('GAE_INSTANCE'):
-        DATABASE_URI = LIVE_SQLALCHEMY_DATABASE_URI
-    else:
-        DATABASE_URI = LOCAL_SQLALCHEMY_DATABASE_URI
-    CLOUD_STORAGE_BUCKET = 'photoshare1234'
     LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO")
+
+    PROJECT_ID = os.environ.get("PROJECT_ID", "photoshare")
+    CLOUD_STORAGE_BUCKET = os.environ.get("CLOUD_STORAGE_BUCKET", "photoshare")
+    DATABASE_USER = os.environ.get("DATABASE_USER", "photoshare")
+    DATABASE_PASSWORD = os.environ.get("DATABASE_USER", "photoshare")
+    DATABASE_NAME = os.environ.get("DATABASE_NAME", "photoshare")
+    DATABASE_HOST = os.environ.get("DATABASE_HOST", "localhost")
+    DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}:3306/{database}'.format(
+        user=DATABASE_USER, password=DATABASE_PASSWORD,
+        host=DATABASE_HOST, database=DATABASE_NAME)
